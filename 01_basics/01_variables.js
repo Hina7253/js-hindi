764b31d49b11a920c8af1295643862daee01b75c
Hina <hinakumari.725394@gmail.com>
2026-02-22 16:41:01 +0530

    Initial Commit

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,7 @@
+const accountId = 12349
+let accountEmail = "hina@google.com"
+let accountPassword = "8647684"
+accountCity = "jaipur"
+
+console.log(accountId)
+console.table([accountId, accountEmail, accountId, accountCity])
